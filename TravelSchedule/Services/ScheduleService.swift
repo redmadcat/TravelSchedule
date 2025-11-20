8866480bd8aff34bf54f1cc5b71268061d8e8a98
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-11-20 15:59:43 +0300

    add: schedule service/proto impl
    Add ScheduleService/ScheduleServiceProtocol implementation.

---
+++ TravelSchedule/Services/ScheduleService.swift
@@ -0,0 +1,18 @@
+//
+//  ScheduleService.swift
+//  TravelSchedule
+//
+//  Created by Roman Yaschenkov on 20.11.2025.
+//
+
+final class ScheduleService: BaseSerivce, ScheduleServiceProtocol {
+    // MARK: - ScheduleServiceProtocol
+    func getStationSchedule(station: String, date: String) async throws -> ScheduleResponse {
+        let response = try await client.getStationSchedule(query: .init(
+            apikey: apikey,
+            station: station,
+            date: date
+        ))
+        return try response.ok.body.json
+    }
+}
---
+++ TravelSchedule/Services/ScheduleServiceProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  ScheduleServiceProtocol.swift
+//  TravelSchedule
+//
+//  Created by Roman Yaschenkov on 20.11.2025.
+//
+
+typealias ScheduleResponse = Components.Schemas.ScheduleResponse
+
+protocol ScheduleServiceProtocol {
+    func getStationSchedule(station: String, date: String) async throws -> ScheduleResponse
+}
