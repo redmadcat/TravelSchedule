b5d5083cc2e9f10bcbd60caa3241b9795327ed84
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-11-26 09:02:42 +0300

    add: api helper member
    Add APIHelper member for responsibility separation.

---
+++ TravelSchedule/Helpers/APIHelper.swift
@@ -0,0 +1,12 @@
+//
+//  APIHelper.swift
+//  TravelSchedule
+//
+//  Created by Roman Yaschenkov on 26.11.2025.
+//
+
+enum Auth {
+    static let apikey = "8b88c1b1-b280-4c1b-8723-46ffd3568ff2"
+}
+
+
