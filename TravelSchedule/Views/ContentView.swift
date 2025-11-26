a1b4fb5c1c613ec7a172dde7c932327d9a5bf5a4
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-11-26 12:07:01 +0300

    add: test timetable service member impl
    Add testTimetableService implementation.

--- TravelSchedule/Views/ContentView.swift
+++ TravelSchedule/Views/ContentView.swift
@@ -7,10 +7,8 @@
 
 import SwiftUI
 import OpenAPIURLSession
-
+ 
 struct ContentView: View {
-    private let apikey = "8b88c1b1-b280-4c1b-8723-46ffd3568ff2"
-    
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,49 +18,45 @@ struct ContentView: View {
         }
         .padding()
         .onAppear {
-            testFetchStations()
-            testFetchCopyright()
+            testTimetableService()
         }
     }
     
-    func testFetchStations() {
+    func testTimetableService() {
         Task {
             do {
                 let client = Client(
                     serverURL: try Servers.Server1.url(),
+                    configuration: .init(dateTranscoder: ISO8601DateTranscoder()),
                     transport: URLSessionTransport()
                 )
+                let timetableService = TimetableService(client: client, apikey: Auth.apikey)
                 
-                let service = NearestStationsService(client: client, apikey: apikey)
+                let nearestStations = try await timetableService.getNearestStations(lat: 59.864177, lng: 30.319163, distance: 50)
+                print("Successfully fetched stations:\n\(nearestStations)")
+
+                let search = try await timetableService.search(from: "c146", to: "c213")
+                print("Successfully fetched search:\n\(search)")
                 
-                print("Fetching stations...")
-                let stations = try await service.getNearestStations(
-                    lat: 59.864177,
-                    lng: 30.319163,
-                    distance: 50
-                )
-                print("Successfully fetched stations: \(stations)")
+                let schedule = try await timetableService.getStationSchedule(station: "s9600213")
+                print("Successfully fetched schedule:\n\(schedule)")
+
+                let thread = try await timetableService.getRouteStations(uid: "DP-6913_251202_c9144_12")
+                print("Successfully fetched thread:\n\(thread)")
+                
+                let nearestCity = try await timetableService.getNearestCity(lat: 59.864177, lng: 30.319163)
+                print("Successfully fetched city:\n\(nearestCity)")
+                
+                let carrier = try await timetableService.getCarrierInfo(code: "63438")
+                print("Successfully fetched carrier:\n\(carrier)")
+                
+                let allStations = try await timetableService.getAllStations().countries?.count
+                print("Successfully fetched allStations count:\n\(String(describing: allStations))")
+                                                                
+                let copyright = try await timetableService.getCopyright()
+                print("Successfully fetched copyright:\n\(copyright)")
             } catch {
-                print("Error fetching stations: \(error)")
-            }
-        }
-    }
-    
-    func testFetchCopyright() {
-        Task {
-            do {
-                let client = Client(
-                    serverURL: try Servers.Server1.url(),
-                    transport: URLSessionTransport()
-                )
-                
-                let service = CopyrightService(client: client, apikey: apikey)
-                
-                print("Fetching copyright...")
-                let copyright = try await service.getCopyright()
-                print("Successfully fetched copyright: \(copyright)")
-            } catch {
-                print("Error fetching stations: \(error)")
+                print("Error: \(error)")
             }
         }
     }
