b40d22e55afaa4b1bccc6a7eebd143b2f2e01af2
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-12-16 09:49:36 +0300

    add: stories view stub
    Add ScheduleStoriesView stub.

---
+++ TravelSchedule/Views/Schedule/ScheduleStoriesView.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleStoriesView.swift
+//  TravelSchedule
+//
+//  Created by Roman Yaschenkov on 15.12.2025.
+//
+
+import SwiftUI
+
+struct ScheduleStoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            
+        }
+        .fixedSize(horizontal: false, vertical: true)
+    }
+}
