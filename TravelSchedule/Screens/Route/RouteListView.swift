01a5775e011d553a9640574e44616291bab12f76
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-12-17 15:55:41 +0300

    add: route list view stub
    Add RouteListView stub.

---
+++ TravelSchedule/Screens/Route/RouteListView.swift
@@ -0,0 +1,14 @@
+//
+//  RouteListView.swift
+//  TravelSchedule
+//
+//  Created by Roman Yaschenkov on 17.12.2025.
+//
+
+import SwiftUI
+
+struct RouteListView: View {
+    var body: some View {
+        Text(verbatim: "RouteList")
+    }
+}
