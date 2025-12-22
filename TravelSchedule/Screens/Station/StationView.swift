eb1cbfeefd642afefe9a0d8d2e6664a462b75760
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-12-22 11:44:05 +0300

    add: station view def
    Add StationView definition.

---
+++ TravelSchedule/Screens/Station/StationView.swift
@@ -0,0 +1,19 @@
+//
+//  StationView.swift
+//  TravelSchedule
+//
+//  Created by Roman Yaschenkov on 22.12.2025.
+//
+
+import SwiftUI
+
+typealias Settlement = Components.Schemas.Settlement
+typealias Station = Components.Schemas.Station
+
+struct StationView: View {
+    let settlement: Settlement
+    
+    var body: some View {
+        Spacer()
+    }
+}
