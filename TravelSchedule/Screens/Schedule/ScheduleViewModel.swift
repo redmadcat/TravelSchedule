838faa445a4ad09bb407028ae1cba7080eccf22e
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-12-23 08:26:34 +0300

    add: base class inheritance
    Add base class inheritance.

--- TravelSchedule/Screens/Schedule/ScheduleViewModel.swift
+++ TravelSchedule/Screens/Schedule/ScheduleViewModel.swift
@@ -5,13 +5,7 @@
 //  Created by Roman Yaschenkov on 17.12.2025.
 //
 
-final class ScheduleViewModel {
-    private let router: Router
-    
-    init(router: Router) {
-        self.router = router
-    }
-    
+final class ScheduleViewModel: RouterViewModel {
     func toSettlements() {
         self.router.toSettlements()
     }
