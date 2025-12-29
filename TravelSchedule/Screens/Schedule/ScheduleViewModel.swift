5004fd6fddb8bdf17e8544c5c3eca3d63c15ebcf
Roman Yaschenkov <ryaschenkov@yandex.ru>
2025-12-29 09:15:43 +0300

    add: schedule vm init member
    Add ScheduleViewModel initializer.

--- TravelSchedule/Screens/Schedule/ScheduleViewModel.swift
+++ TravelSchedule/Screens/Schedule/ScheduleViewModel.swift
@@ -5,8 +5,14 @@
 //  Created by Roman Yaschenkov on 17.12.2025.
 //
 
-final class ScheduleViewModel: RouterViewModel {
-    func toSettlements() {
-        self.router.toSettlements()
+final class ScheduleViewModel {
+    var route: RouteFinderViewModel
+    
+    init(route: RouteFinderViewModel, settlement: Settlement?, station: Station?) {
+        self.route = route
+        
+        guard let settlementTitle = settlement?.title, let stationTitle = station?.title else { return }
+        self.route.from.text = settlementTitle + " (" + stationTitle + ")"
     }
 }
+
