f9e12574c766961a38e58a72399dafdefeb1b03a
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-13 14:45:44 +0300

    add: carrier view impl
    Add CarrierView implementation.

--- TravelSchedule/Screens/Carrier/CarrierView.swift
+++ TravelSchedule/Screens/Carrier/CarrierView.swift
@@ -8,17 +8,59 @@
 import SwiftUI
 
 struct CarrierView: View {
+    @Environment(\.dismiss) private var dismiss
+    @State var context: CarrierViewModel
     let route: Route
+    private var segments: [Segment] {
+        context.segments
+    }
     
     var body: some View {
-        VStack {
-            Text(verbatim: "CarrierView")
+        ZStack {
+            Color.ypWhiteAD.ignoresSafeArea()
+            
+            VStack {
+                switch context.status {
+                case .loading:
+                    ProgressLoadingView()
+                case .success, .default:
+                    VStack {
+                        segments.isEmpty ?
+                            AnyView(SearchStubView(text: "NoOptions")) :
+                            AnyView(CarrierListView(segments: segments))
+                    }
+                case .failure(let error):
+                    error == .network ? ErrorView(text: "NoInternet") : ErrorView(text: "ServerError")
+                }
+            }
+            .safeAreaInset(edge: .top) {
+                VStack {
+                    Text(route.description())
+                        .font(.system(size: 24, weight: .bold))
+                        .foregroundColor(.ypBlackAD)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                }
+                .background(.ypWhiteAD)
+            }
+            .padding(.horizontal, 16)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-        .background(.ypWhiteAD)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarLeading) {
+                Button(action: {
+                    dismiss()
+                }) {
+                    Image(systemName: "chevron.left")
+                        .foregroundColor(.ypBlackAD)
+                }
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+        .task {
+            await context.load(route: route)
+        }
     }
 }
 
 #Preview {
-    CarrierView(route: Route())
+    CarrierBuilder().build(route: Route())
 }
