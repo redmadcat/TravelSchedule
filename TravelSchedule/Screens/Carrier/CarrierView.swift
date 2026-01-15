b9bbf3d61ea90faec6c8abdef3eaa87592a71ac4
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-16 00:41:13 +0300

    fix: time spec button position/filter behavior
    Fix time specify button posigion and filter behavior.

--- TravelSchedule/Screens/Carrier/CarrierView.swift
+++ TravelSchedule/Screens/Carrier/CarrierView.swift
@@ -10,8 +10,11 @@ import SwiftUI
 struct CarrierView: View {
     @Environment(\.dismiss) private var dismiss
     @State var context: CarrierViewModel
+    @State var filter: CarrierFilter
     let route: Route
     private var segments: [Segment] {
+        filter.isApplied ?
+        context.segments.filter { filter.apply($0) } :
         context.segments
     }
     
@@ -33,9 +36,27 @@ struct CarrierView: View {
                     error == .network ? ErrorView(text: "NoInternet") : ErrorView(text: "ServerError")
                 }
             }
+            .safeAreaInset(edge: .bottom) {
+                Button {
+                    Router.shared.toFilter(filter: filter)
+                } label: {
+                    HStack {
+                        Text("SpecifyTime")
+                            .font(.system(size: 17, weight: .bold))
+                            .foregroundStyle(.ypWhite)
+                        Circle().frame(width: 10, height: 10, alignment: .leading)
+                            .padding(.top, 3)
+                            .foregroundColor(filter.isApplied ? .ypRed : .clear)
+                    }
+                    .frame(maxWidth: .infinity, minHeight: 60)
+                }
+                .background(RoundedRectangle(cornerRadius: 16).fill(.ypBlue))
+                .padding(.bottom, 24)
+            }
             .safeAreaInset(edge: .top) {
                 VStack {
                     Text(route.description())
+                        .padding(.top, 10)
                         .font(.system(size: 24, weight: .bold))
                         .foregroundColor(.ypBlackAD)
                         .frame(maxWidth: .infinity, alignment: .leading)
@@ -47,6 +68,7 @@ struct CarrierView: View {
         .toolbar {
             ToolbarItem(placement: .navigationBarLeading) {
                 Button(action: {
+                    filter.isApplied = false
                     dismiss()
                 }) {
                     Image(systemName: "chevron.left")
@@ -56,7 +78,9 @@ struct CarrierView: View {
         }
         .navigationBarBackButtonHidden(true)
         .task {
-            await context.load(route: route)
+            if !filter.isApplied {
+                await context.load(route: route)
+            }
         }
     }
 }
