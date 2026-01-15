f19f3720ab6c4a4cb4b6384dc951a6b3b37896ef
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-16 00:45:46 +0300

    add: carrier filter view impl
    Add CarrierFilterView implementation.

--- TravelSchedule/Screens/Carrier/CarrierFilterView.swift
+++ TravelSchedule/Screens/Carrier/CarrierFilterView.swift
@@ -10,90 +10,99 @@ import SwiftUI
 struct CarrierFilterView: View {
     @Environment(\.dismiss) private var dismiss
     @State private var isEnabled = false
+    @State var filter: CarrierFilter
     
     var body: some View {
-        VStack {
-            ScrollView {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("DepartureTime")
-                        .padding(.vertical, 8)
-                        .font(.system(size: 24, weight: .bold))
-                    VStack(spacing: 0) {
-                        ForEach(Departure.allCases) { departure in
-                            Button {
-                                
-                            } label: {
-                                HStack {
-                                    Text(NSLocalizedString(departure.rawValue, comment: .empty))
-                                    Spacer()
-                                    Image(systemName: "square")
-                                        .font(.system(size: 20))
-                                }
-                                .frame(height: 60)
-                            }
-                            .buttonStyle(.plain)
-                        }
-                    }
-                }
-                .foregroundStyle(.ypBlackAD)
-                
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("ShowWithTransfers")
-                        .padding(.vertical, 8)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .font(.system(size: 24, weight: .bold))
-                    VStack(spacing: 0) {
-                        ForEach(Transfer.allCases) { transfer in
-                            Button {
-                                
-                            } label: {
-                                HStack {
-                                    Text(NSLocalizedString(transfer.rawValue, comment: .empty))
-                                    Spacer()
-                                    Image(systemName: "circle")
-                                        .font(.system(size: 20))
-                                }
-                                .frame(height: 60)
-                            }
-                            .buttonStyle(.plain)
-                        }
-                    }
-                }
-            }
-            .scrollIndicators(.hidden)
-            .scrollContentBackground(.hidden)
-            
-            Spacer()
+        ZStack {
+            Color.ypWhiteAD.ignoresSafeArea()
             
             VStack {
-                Button {
-                    dismiss()
-                } label: {
-                    Text("Apply")
-                        .font(.system(size: 17, weight: .bold))
-                        .foregroundStyle(.ypWhite)
-                        .frame(maxWidth: .infinity, minHeight: 60)
-                        .contentShape(Rectangle())
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("DepartureTime")
+                            .padding(.vertical, 10)
+                            .font(.system(size: 24, weight: .bold))
+                        VStack(spacing: 0) {
+                            ForEach(Departure.allCases) { departure in
+                                Button {
+                                    filter.toggle(departure)
+                                } label: {
+                                    HStack {
+                                        Text(NSLocalizedString(departure.rawValue, comment: .empty))
+                                            .font(.system(size: 17, weight: .regular))
+                                        Spacer()
+                                        Image(systemName: filter.toggled(departure) ? "checkmark.square.fill" : "square")
+                                            .font(.system(size: 20))
+                                    }
+                                    .frame(height: 60)
+                                }
+                                .buttonStyle(.plain)
+                            }
+                        }
+                    }
+                    .foregroundStyle(.ypBlackAD)
+                    
+                    VStack(alignment: .leading, spacing: 0) {
+                        Text("ShowWithTransfers")
+                            .padding(.vertical, 10)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .font(.system(size: 24, weight: .bold))
+                        VStack(spacing: 0) {
+                            ForEach(Transfer.allCases) { transfer in
+                                Button {
+                                    filter.toggle(transfer)
+                                } label: {
+                                    HStack {
+                                        Text(NSLocalizedString(transfer.rawValue, comment: .empty))
+                                            .font(.system(size: 17, weight: .regular))
+                                        Spacer()
+                                        Image(systemName: filter.toggled(transfer) ? "largecircle.fill.circle" : "circle")
+                                            .font(.system(size: 20))
+                                    }
+                                    .frame(height: 60)
+                                }
+                                .buttonStyle(.plain)
+                            }
+                        }
+                    }
                 }
-                .background(RoundedRectangle(cornerRadius: 20).fill(.ypBlue))
-                .padding(.bottom, 24)
+                .scrollIndicators(.hidden)
+                .scrollContentBackground(.hidden)
+                
+                Spacer()
+                
+                VStack {
+                    Button {
+                        filter.set()
+                        dismiss()
+                    } label: {
+                        Text("Apply")
+                            .font(.system(size: 17, weight: .bold))
+                            .foregroundStyle(.ypWhite)
+                            .frame(maxWidth: .infinity, minHeight: 60)
+                            .contentShape(Rectangle())
+                    }
+                    .background(RoundedRectangle(cornerRadius: 16).fill(.ypBlue))
+                    .padding(.bottom, 24)
+                }
+                .opacity(filter.isSet() ? 1 : 0)
             }
-        }
-        .padding(.horizontal, 16)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: {
-                    dismiss()
-                }) {
-                    Image(systemName: "chevron.left")
-                        .foregroundColor(.ypBlackAD)
+            .padding(.horizontal, 16)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button(action: {
+                        dismiss()
+                    }) {
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.ypBlackAD)
+                    }
                 }
             }
+            .navigationBarBackButtonHidden(true)
         }
-        .navigationBarBackButtonHidden(true)
     }
 }
 
 #Preview {
-    CarrierFilterView()
+    CarrierFilterView(filter: CarrierFilter())
 }
