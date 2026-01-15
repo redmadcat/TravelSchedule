8578fe92be4819e23bbf8e19dab730646644e25e
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-16 00:32:14 +0300

    refactor: remove deprecated props
    Refactor, remove deprecated properties.

--- TravelSchedule/Extensions/ExtensionSegment.swift
+++ TravelSchedule/Extensions/ExtensionSegment.swift
@@ -7,60 +7,39 @@
 
 import Foundation
 
+typealias TicketsInfo = Components.Schemas.TicketsInfo
+
 extension Segment {
-    var departureDate: Date {
-        let time = getRandomTime()
-        return Calendar.current.date(bySettingHour: time.hour, minute: time.minute, second: 00, of: Date())!
-    }
-    
     var fakeHasTransfers: Bool {
-        return Bool.random()
+        return tickets_info != nil ? true : false
     }
     
     var fakeDepartureDate: String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ru_RU")
         formatter.dateFormat = "d MMMM"
-        return formatter.string(from: departureDate)
+        return formatter.string(from: departure ?? Date())
     }
     
     var fakeDepartureTime: String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ru_RU")
         formatter.dateFormat = "HH:mm"
-        return formatter.string(from: departureDate)
+        return formatter.string(from: departure ?? Date())
     }
     
-    var durationFromFakeDeparture: String {
-        guard let duration = duration else { return .dash }
+    var durationFromFakeDeparture: Int {
+        guard let duration = duration else { return 0 }
         let hours = duration / 3600
-        let minutes = (duration % 3600) / 60
-        
-        return hours > 0 ?
-            "\(hours) \(NSLocalizedString("h", comment: .empty)) \(minutes) \(NSLocalizedString("min", comment: .empty))" :
-            "\(minutes) \(NSLocalizedString("min", comment: .empty))"
+        return hours
     }
     
-    var arrivalText: String {
+    var arrivalTime: String {
         guard let duration = duration else { return .dash }
-        let arrival = Date().addingTimeInterval(Double(duration))
+        let arrival = (departure ?? Date()).addingTimeInterval(Double(duration))
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ru_RU")
         formatter.dateFormat = "HH:mm"
         return formatter.string(from: arrival)
-    }
-    
-    func getRandomTime() -> (hour: Int, minute: Int) {
-        let array = [
-            (22, 30),
-            (21, 45),
-            (01, 15),
-            (05, 50),
-            (13, 30),
-            (13, 45),
-            (19, 45)
-        ]
-        
-        return array.randomElement()!
     }
 }
