135361d57f610626a7a4ad2ddc909bd63fd10a49
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-16 00:35:31 +0300

    add: carrier filter impl
    Add CarrierFilter implementation.

---
+++ TravelSchedule/Screens/Carrier/CarrierFilter.swift
@@ -0,0 +1,66 @@
+//
+//  CarrierFilter.swift
+//  TravelSchedule
+//
+//  Created by Roman Yaschenkov on 15.01.2026.
+//
+
+import UIKit
+
+@Observable
+final class CarrierFilter: Hashable {
+    var departures: Set<Departure> = []
+    var transfers: Transfer?
+    var isApplied = false
+    
+    func toggle(_ departure: Departure) {
+        if departures.contains(departure) {
+            departures.remove(departure)
+        } else {
+            departures.insert(departure)
+        }
+    }
+    
+    func toggle(_ transfers: Transfer) {
+        self.transfers = transfers
+    }
+    
+    func apply(_ segment: Segment) -> Bool {
+        let time = departures.contains { $0.isInBetween(departure: segment.departure ?? Date()) }
+        
+        let transfer: Bool = {
+            switch transfers {
+            case .yes: return segment.fakeHasTransfers
+            case .no: return !segment.fakeHasTransfers
+            default:
+                return false
+            }
+        }()
+        
+        return time && transfer
+    }
+    
+    func toggled(_ departure: Departure) -> Bool {
+        return departures.contains(departure)
+    }
+    
+    func toggled(_ transfers: Transfer) -> Bool {
+        return self.transfers == transfers
+    }
+    
+    static func == (lhs: CarrierFilter, rhs: CarrierFilter) -> Bool {
+        ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(ObjectIdentifier(self))
+    }
+    
+    func isSet() -> Bool {
+        return !departures.isEmpty && transfers != nil
+    }
+    
+    func set() {
+        isApplied = !departures.isEmpty
+    }
+}
