382a3b0ebbcf8d84b450b383a3e9387719363b25
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-16 00:39:07 +0300

    add: segment creation helper members
    Add Segment creation helper members to fill unavailable info.

--- TravelSchedule/Screens/Carrier/CarrierViewModel.swift
+++ TravelSchedule/Screens/Carrier/CarrierViewModel.swift
@@ -31,8 +31,17 @@ final class CarrierViewModel {
             isBusy = true
                                     
             let response = try await service.search(from: fromStationCode, to: toStationCode)
-            
-            let segments = response.segments ?? []
+                        
+            for segment in response.segments ?? [] {
+                let result = Segment(from: segment.from,
+                                     to: segment.to,
+                                     departure: getRandomTime(),
+                                     arrival: Date(),
+                                     thread: segment.thread,
+                                     tickets_info: getRandomInfo(),
+                                     duration: segment.duration)
+                segments.append(result)
+            }
             
             self.segments = segments.sorted { lhs, rhs in
                 if let lhsDate = lhs.departure, let rhsDate = rhs.departure {
@@ -46,5 +55,28 @@ final class CarrierViewModel {
         } catch {
             status = error is URLError ? .failure(.network) : .failure(.server)
         }
+    }
+    
+    func getRandomInfo() -> TicketsInfo? {
+        let array = [
+            TicketsInfo(),
+            nil
+        ]
+        return array.randomElement()!
+    }
+    
+    func getRandomTime() -> Date {
+        let array = [
+            (22, 30),
+            (21, 45),
+            (01, 15),
+            (05, 50),
+            (13, 30),
+            (13, 45),
+            (19, 45)
+        ]
+        
+        let time = array.randomElement()!
+        return Calendar.current.date(bySettingHour: time.0, minute: time.1, second: 00, of: Date())!
     }
 }
