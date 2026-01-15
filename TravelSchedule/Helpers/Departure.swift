2cd6e33c3bfc4e46c6da6a4007f54c857335bc4d
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-16 00:33:32 +0300

    fix: simplify hour comparison swich impl
    Fix, simplify hour comparison switch implementation.

--- TravelSchedule/Helpers/Departure.swift
+++ TravelSchedule/Helpers/Departure.swift
@@ -5,6 +5,8 @@
 //  Created by Roman Yaschenkov on 14.01.2026.
 //
 
+import Foundation
+
 enum Departure: String, CaseIterable, Identifiable {
     case morning = "Morning"
     case day = "Day"
@@ -12,4 +14,19 @@ enum Departure: String, CaseIterable, Identifiable {
     case night = "Night"
     
     var id: String { rawValue }
+    
+    func isInBetween(departure: Date) -> Bool {
+        let hour = Calendar.current.component(.hour, from: departure)
+        
+        switch self {
+        case .morning:
+            return (6..<12).contains(hour)
+        case .day:
+            return (12..<18).contains(hour)
+        case .evening:
+            return (18..<24).contains(hour)
+        case .night:
+            return (0..<6).contains(hour)
+        }
+    }
 }
