2d8a31f8945a09bee24f4979af9faa0741e96363
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-20 14:06:03 +0300

    refactor: progress bar impl
    Refactor ProgressBarView layout implementation.

--- TravelSchedule/Screens/Progress/ProgressBarView.swift
+++ TravelSchedule/Screens/Progress/ProgressBarView.swift
@@ -7,12 +7,65 @@
 
 import SwiftUI
 
+extension CGFloat {
+    static let progressBarCornerRadius: CGFloat = 6
+    static let progressBarHeight: CGFloat = 6
+}
+
 struct ProgressBarView: View {
+    let numberOfSections: Int
+    let progress: CGFloat
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                RoundedRectangle(cornerRadius: .progressBarCornerRadius)
+                    .frame(width: geometry.size.width, height: .progressBarHeight)
+                    .foregroundColor(.ypWhite)
+
+                RoundedRectangle(cornerRadius: .progressBarCornerRadius)
+                    .frame(
+                        width: min(
+                            progress * geometry.size.width,
+                            geometry.size.width
+                        ),
+                        height: .progressBarHeight
+                    )
+                    .foregroundColor(.ypBlue)
+            }
+            .mask {
+                MaskView(numberOfSections: numberOfSections)
+            }
+        }
+    }
+}
+
+private struct MaskView: View {
+    let numberOfSections: Int
+
+    var body: some View {
+        HStack {
+            ForEach(0..<numberOfSections, id: \.self) { _ in
+                MaskFragmentView()
+            }
+        }
+    }
+}
+
+private struct MaskFragmentView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: .progressBarCornerRadius)
+            .fixedSize(horizontal: false, vertical: true)
+            .frame(height: .progressBarHeight)
+            .foregroundStyle(.ypWhite)
     }
 }
 
 #Preview {
-    ProgressBarView()
+    Color.clear
+        .ignoresSafeArea()
+        .overlay(
+            ProgressBarView(numberOfSections: 5, progress: 0.5)
+                .padding()
+        )
 }
