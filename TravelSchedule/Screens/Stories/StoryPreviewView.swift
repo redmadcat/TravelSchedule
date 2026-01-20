6145ae64209662c56a321d6f4da5f2ed693a65ec
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-20 21:22:22 +0300

    fix: story preview overlay stroke border issue
    Fix StoryPreviewView overlay stroke border issue to inner.

--- TravelSchedule/Screens/Stories/StoryPreviewView.swift
+++ TravelSchedule/Screens/Stories/StoryPreviewView.swift
@@ -7,29 +7,29 @@
 
 import SwiftUI
 
-struct StoryPreviewView: View {
+struct StoryPreviewView: View {    
     var story: Story
     
     var body: some View {
-        image.overlay(alignment: .bottomLeading) {
-            Text(story.title)
-                .font(.system(size: 12, weight: .regular))
-                .foregroundStyle(.ypWhite)
-                .opacity(story.viewed ? 0.5 : 1)
-                .lineLimit(3)
-                .padding(.horizontal, 8)
-                .padding(.bottom, 12)
-        }
-    }
-    
-    private var image: some View {
         story.image
             .resizable()
             .scaledToFill()
             .frame(width: 92, height: 140)
             .clipped()
             .clipShape(RoundedRectangle(cornerRadius: 16))
-            .opacity(story.viewed ? 0.5 : 1)
+            .opacity(story.visited ? 0.5 : 1)
+            .overlay(alignment: .bottomLeading) {
+            Text(story.title)
+                .font(.system(size: 12, weight: .regular))
+                .foregroundStyle(.ypWhite)
+                .opacity(story.visited ? 0.5 : 1)
+                .lineLimit(3)
+                .padding(.horizontal, 8)
+                .padding(.bottom, 12)
+        }
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+        .overlay(RoundedRectangle(cornerRadius: 16)
+        .strokeBorder(.ypBlue, lineWidth: !story.visited ? 4 : 0))
     }
 }
 
