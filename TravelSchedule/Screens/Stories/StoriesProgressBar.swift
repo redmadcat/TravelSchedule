2adcf85d971c1ad903ad4d51405c0d43134a09f9
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-20 14:07:31 +0300

    fix: stories progress bar impl
    Fix StoriesProgressBar layout implementation.

---
+++ TravelSchedule/Screens/Stories/StoriesProgressBar.swift
@@ -0,0 +1,51 @@
+//
+//  StoriesProgressBar.swift
+//  TravelSchedule
+//
+//  Created by Roman Yaschenkov on 19.01.2026.
+//
+
+import SwiftUI
+import Combine
+
+struct StoriesProgressBar: View {
+    let storiesCount: Int
+    let timerConfiguration: TimerConfiguration
+    @Binding var currentProgress: CGFloat
+    @State private var timer: Timer.TimerPublisher
+    @State private var cancellable: Cancellable?
+    
+    init(storiesCount: Int, timerConfiguration: TimerConfiguration, currentProgress: Binding<CGFloat>) {
+        self.storiesCount = storiesCount
+        self.timerConfiguration = timerConfiguration
+        self._currentProgress = currentProgress
+        self.timer = Self.makeTimer(configuration: timerConfiguration)
+    }
+        
+    var body: some View {
+        ProgressBarView(numberOfSections: storiesCount, progress: currentProgress)
+            .padding(.init(top: 7, leading: 12, bottom: 12, trailing: 12))
+            .onAppear {
+                timer = Self.makeTimer(configuration: timerConfiguration)
+                cancellable = timer.connect()
+            }
+            .onDisappear {
+                cancellable?.cancel()
+            }
+            .onReceive(timer) { _ in
+                timerTick()
+            }
+    }
+    
+    private func timerTick() {
+        withAnimation {
+            currentProgress = timerConfiguration.nextProgress(progress: currentProgress)
+        }
+    }
+}
+
+extension StoriesProgressBar {
+    private static func makeTimer(configuration: TimerConfiguration) -> Timer.TimerPublisher {
+        Timer.publish(every: configuration.timerTickInternal, on: .main, in: .common)
+    }
+}
