22d183da51faa4408c71447b28ade4a743bc12c0
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-21 23:18:55 +0300

    fix: literal resource access modifier
    Fix: literal resource access modifier to const.

--- TravelSchedule/Helpers/MockStories.swift
+++ TravelSchedule/Helpers/MockStories.swift
@@ -27,41 +27,41 @@ final class MockStories {
     private static let title9 = "Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 Text9 "
         
     static let stories = [
-        Story(visited: false, image: Image("Preview1"), title: title1, stories: [
-            Story(visited: false, image: Image("Story1"), title: title1, stories: []),
-            Story(visited: false, image: Image("Story2"), title: title1, stories: [])
+        Story(visited: false, image: Image(.preview1), title: title1, stories: [
+            Story(visited: false, image: Image(.story1), title: title1, stories: []),
+            Story(visited: false, image: Image(.story2), title: title1, stories: [])
         ]),
-        Story(visited: false, image: Image("Preview2"), title: title2, stories: [
-            Story(visited: false, image: Image("Story3"), title: title2, stories: []),
-            Story(visited: false, image: Image("Story4"), title: title2, stories: [])
+        Story(visited: false, image: Image(.preview2), title: title2, stories: [
+            Story(visited: false, image: Image(.story3), title: title2, stories: []),
+            Story(visited: false, image: Image(.story4), title: title2, stories: [])
         ]),
-        Story(visited: false, image: Image("Preview3"), title: title3, stories: [
-            Story(visited: false, image: Image("Story5"), title: title3, stories: []),
-            Story(visited: false, image: Image("Story6"), title: title3, stories: [])
+        Story(visited: false, image: Image(.preview3), title: title3, stories: [
+            Story(visited: false, image: Image(.story5), title: title3, stories: []),
+            Story(visited: false, image: Image(.story6), title: title3, stories: [])
         ]),
-        Story(visited: false, image: Image("Preview4"), title: title4, stories: [
-            Story(visited: false, image: Image("Story7"), title: title4, stories: []),
-            Story(visited: false, image: Image("Story8"), title: title4, stories: [])
+        Story(visited: false, image: Image(.preview4), title: title4, stories: [
+            Story(visited: false, image: Image(.story7), title: title4, stories: []),
+            Story(visited: false, image: Image(.story8), title: title4, stories: [])
         ]),
-        Story(visited: false, image: Image("Preview5"), title: title5, stories: [
-            Story(visited: false, image: Image("Story9"), title: title5, stories: []),
-            Story(visited: false, image: Image("Story10"), title: title5, stories: [])
+        Story(visited: false, image: Image(.preview5), title: title5, stories: [
+            Story(visited: false, image: Image(.story9), title: title5, stories: []),
+            Story(visited: false, image: Image(.story10), title: title5, stories: [])
         ]),
-        Story(visited: false, image: Image("Preview6"), title: title6, stories: [
-            Story(visited: false, image: Image("Story11"), title: title6, stories: []),
-            Story(visited: false, image: Image("Story12"), title: title6, stories: [])
+        Story(visited: false, image: Image(.preview6), title: title6, stories: [
+            Story(visited: false, image: Image(.story11), title: title6, stories: []),
+            Story(visited: false, image: Image(.story12), title: title6, stories: [])
         ]),
-        Story(visited: false, image: Image("Preview7"), title: title7, stories: [
-            Story(visited: false, image: Image("Story13"), title: title7, stories: []),
-            Story(visited: false, image: Image("Story14"), title: title7, stories: [])
+        Story(visited: false, image: Image(.preview7), title: title7, stories: [
+            Story(visited: false, image: Image(.story13), title: title7, stories: []),
+            Story(visited: false, image: Image(.story14), title: title7, stories: [])
         ]),
-        Story(visited: false, image: Image("Preview8"), title: title8, stories: [
-            Story(visited: false, image: Image("Story15"), title: title8, stories: []),
-            Story(visited: false, image: Image("Story16"), title: title8, stories: [])
+        Story(visited: false, image: Image(.preview8), title: title8, stories: [
+            Story(visited: false, image: Image(.story15), title: title8, stories: []),
+            Story(visited: false, image: Image(.story16), title: title8, stories: [])
         ]),
-        Story(visited: false, image: Image("Preview9"), title: title9, stories: [
-            Story(visited: false, image: Image("Story17"), title: title9, stories: []),
-            Story(visited: false, image: Image("Story18"), title: title9, stories: [])
+        Story(visited: false, image: Image(.preview9), title: title9, stories: [
+            Story(visited: false, image: Image(.story17), title: title9, stories: []),
+            Story(visited: false, image: Image(.story18), title: title9, stories: [])
         ])
     ]
 }
