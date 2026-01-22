c42bb342f9bb9e88588f7e071cf4befdd624054b
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-23 01:22:02 +0300

    refactor: segment extensiond date/time members
    Refactor Segment extension date/time members.

--- TravelSchedule/Extensions/ExtensionSegment.swift
+++ TravelSchedule/Extensions/ExtensionSegment.swift
@@ -7,39 +7,52 @@
 
 import Foundation
 
-typealias TicketsInfo = Components.Schemas.TicketsInfo
-
 extension Segment {
-    var fakeHasTransfers: Bool {
-        return tickets_info != nil ? true : false
+    var hasTransfers: Bool {
+        guard let has_transfers else { return false }
+        return has_transfers
     }
     
-    var fakeDepartureDate: String {
+    var departureDate: Date {
+        guard let departure else { return Date() }
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ru_RU")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        return formatter.date(from: departure) ?? Date()
+    }
+    
+    var arrivalDate: Date {
+        guard let arrival else { return Date() }
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ru_RU")
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        return formatter.date(from: arrival) ?? Date()
+    }
+        
+    var departureDay: String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ru_RU")
         formatter.dateFormat = "d MMMM"
-        return formatter.string(from: departure ?? Date())
+        return formatter.string(from: departureDate)
     }
     
-    var fakeDepartureTime: String {
+    var departureTime: String {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ru_RU")
         formatter.dateFormat = "HH:mm"
-        return formatter.string(from: departure ?? Date())
-    }
-    
-    var durationFromFakeDeparture: Int {
-        guard let duration = duration else { return 0 }
-        let hours = duration / 3600
-        return hours
+        return formatter.string(from: departureDate)
     }
     
     var arrivalTime: String {
-        guard let duration = duration else { return .dash }
-        let arrival = (departure ?? Date()).addingTimeInterval(Double(duration))
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ru_RU")
         formatter.dateFormat = "HH:mm"
-        return formatter.string(from: arrival)
+        return formatter.string(from: arrivalDate)
+    }
+    
+    var durationHours: Int {
+        guard let duration = duration else { return 0 }
+        let hours = duration / 3600
+        return hours
     }
 }
