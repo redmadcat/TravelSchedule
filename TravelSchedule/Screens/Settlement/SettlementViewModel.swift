192b63bd967fc2e892e4ab9240bc0f0e844c0057
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-22 11:42:31 +0300

    fix: settlement view model load member impl
    Fix SettlementViewModel load member implementation.

--- TravelSchedule/Screens/Settlement/SettlementViewModel.swift
+++ TravelSchedule/Screens/Settlement/SettlementViewModel.swift
@@ -9,10 +9,8 @@ import SwiftUI
 
 @Observable
 final class SettlementViewModel {
-    private let country = "Россия"
     private let service: StationsServiceProtocol
     private var settlementsRaw: [Settlement] = []
-    var isBusy: Bool = false
     var request: String = .empty
     var direction: Route.Direction
     var status: APIResponseStatus = .default
@@ -31,24 +29,9 @@ final class SettlementViewModel {
     }
     
     func load() async {
-        defer { isBusy = false }
-                
         do {
             status = .loading
-            isBusy = true
-            let response = try await service.getAllStations()
-            if let result = response.countries?.filter({ $0.title == country }).first {
-                if let regions = result.regions {
-                    // Fetched most appropriate regions for test purpose
-                    if let regionMsk = regions[37].settlements, let regionSpb = regions[53].settlements {
-                        settlementsRaw = regionMsk + regionSpb
-                            .filter {
-                                guard let title = $0.title else { return false }
-                                return !title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-                            }
-                    }
-                }
-            }
+            settlementsRaw = try await service.getAllStations()
             status = .success
         } catch {
             status = error is URLError ? .failure(.network) : .failure(.server)
