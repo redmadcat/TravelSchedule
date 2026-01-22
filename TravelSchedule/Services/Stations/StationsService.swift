61308f485f136057c116fe85c8e6e34df1610a2a
Roman Yaschenkov <ryaschenkov@yandex.ru>
2026-01-23 01:19:16 +0300

    fix: include all regions to result dataset
    Fix, include all regions to result dataset.

--- TravelSchedule/Services/Stations/StationsService.swift
+++ TravelSchedule/Services/Stations/StationsService.swift
@@ -18,28 +18,31 @@ final class StationsService: StationsServiceProtocol {
     }
     
     func getAllStations() async throws -> [Settlement] {
+        var settlements: [Settlement] = []
         let response = try await client.getAllStations(query: .init(apikey: apiKey))
-        
         let responseBody = try response.ok.body.text_html_charset_utf_hyphen_8
         
         let limit = 50 * 1024 * 1024
         let fullData = try await Data(collecting: responseBody, upTo: limit)
     
-        let allStations = try JSONDecoder().decode(AllStationsResponse.self, from: fullData)
-                
-        if let result = allStations.countries?.filter({ $0.title == country }).first {
-            if let regions = result.regions {
-                // Fetched most appropriate regions for test purpose
-                if let regionMsk = regions[37].settlements, let regionSpb = regions[53].settlements {
-                    return regionMsk + regionSpb
-                        .filter {
-                            guard let title = $0.title else { return false }
-                            return !title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
-                        }
+        let stationsResponse = try JSONDecoder().decode(AllStationsResponse.self, from: fullData)
+                            
+        guard let result = stationsResponse.countries?.filter({ $0.title == country }).first else { return [] }
+        if let regions = result.regions {
+            for region in regions {
+                if let regionSettlements = region.settlements {
+                    settlements += regionSettlements
                 }
             }
         }
         
-        return []
+        return settlements.filter {
+            guard let title = $0.title else { return false }
+            return !title.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
+        }
+        .sorted {
+            guard let title1 = $0.title, let title2 = $1.title else { return false }
+            return title1 < title2
+        }
     }
 }
